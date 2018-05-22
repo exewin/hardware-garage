91f9651c7e222bca5cc6d8905b694b94f98acba5
exewin <windowsmemphis@gmail.com>
2018-05-22 11:55:13 +0200

    replaced images
    old images removed due to copyright

--- footer.php
+++ footer.php
@@ -3,6 +3,7 @@
 <br/>
 <a href="phpmyadmin" target="_blank">phpMyAdmin</a>
 <a href="clearlogs.php">Clear Log</a>
+<a href="https://icons8.com">Icon pack by Icons8</a>
 <br/>
 <br/>
 Copyright © 2017 Exewin
